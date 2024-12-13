310217ced77410632ecddcebc2674d93b39bf029
Sriram Sridharan <s.sriram@yethi.in>
2024-12-13 17:49:30 +0530

    End of lecture 36

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 from langchain import hub
@@ -10,6 +11,9 @@ from langchain.chains.retrieval import create_retrieval_chain
 
 
 load_dotenv()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 if __name__ == "__main__":
     print("retrieving...")
@@ -23,12 +27,34 @@ if __name__ == "__main__":
         index_name=os.environ["INDEX_NAME"], embedding=embeddings
     )
 
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    # retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    # combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    #
+    # retrieval_chain = create_retrieval_chain(
+    #     retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    # )
+    #
+    # result = retrieval_chain.invoke(input={"input": query})
+    # print(result)
 
-    retrieval_chain = create_retrieval_chain(
-        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    template = """
+        Use the following pieces of context to answer the question at the end.
+        If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+        Use three sentences maximum, and keep the answer as concise as possible.
+        Always say "Thanks for asking!" at the end of the answer.
+        
+        {context}
+        
+        Question: {question}
+        
+        Helpful Answer: 
+    """
+    custom_rag_prompt = PromptTemplate.from_template(template)
+    rag_chain = (
+        {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
     )
 
-    result = retrieval_chain.invoke(input={"input": query})
-    print(result)
+    res = rag_chain.invoke(query)
+    print(res)
